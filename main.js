6be59c443eabb94c3a390c32996902a32eac0e40
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-01-12 00:11:54 +0100

    Terminato esercizio con switch. Terminato Selfwork Condizioni 2.

--- main.js
+++ main.js
@@ -6,23 +6,50 @@
 //se temperatura è minore di -10, stampare “copriti…ancora ti raffreddi”
 //Cerca di risolvere questo esercizio utilizzando prima if else e poi con switch case.
 
+// ------- IF / ELSE -------
+
+// let temperature = prompt('Inserisci la temperatura: ');
+
+// if(temperature < -10){
+//     console.log('Copriti…ancora ti raffreddi');   
+// }
+// else if(temperature < 0){
+//     console.log(`Non è tanto il freddo quanto l'umidità`);   
+// }
+// else if(temperature < 20){
+//     console.log('Non ci sono più le mezze stagioni');   
+// }
+// else if(temperature < 30){
+//     console.log('Mi dia una peroni sudata');   
+// }
+// else if(temperature >= 30){
+//     console.log('Lu mare, lu sole, lu ientu');   
+// }
+// else {
+//     console.log('Valore non valido');
+// }
+
+// ------- SWITCH -------
+
 let temperature = prompt('Inserisci la temperatura: ');
 
-if(temperature < -10){
-    console.log('Copriti…ancora ti raffreddi');   
-}
-else if(temperature < 0){
-    console.log(`Non è tanto il freddo quanto l'umidità`);   
-}
-else if(temperature < 20){
-    console.log('Non ci sono più le mezze stagioni');   
-}
-else if(temperature < 30){
-    console.log('Mi dia una peroni sudata');   
-}
-else if(temperature >= 30){
-    console.log('Lu mare, lu sole, lu ientu');   
-}
-else {
-    console.log('Valore non valido');
+switch(true) {
+    case temperature < -10:
+        console.log('Copriti…ancora ti raffreddi');   
+        break;
+    case temperature < 0:
+        console.log(`Non è tanto il freddo quanto l'umidità`);   
+        break;
+    case temperature < 20:
+        console.log('Non ci sono più le mezze stagioni');   
+        break;
+    case temperature < 30:
+        console.log('Mi dia una peroni sudata');   
+        break;
+    case temperature >= 30:
+        console.log('Lu mare, lu sole, lu ientu');   
+        break;
+    default:
+        console.log('Valore non valido');
+        break;
 }
